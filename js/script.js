b085998f8e90884fb04f6b524e47f739ca2d3949
Lee David <leedav87@gmail.com>
2021-08-29 16:43:02 -0300

    arrays de objetos hoteles

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const hoteles = [{
+        hotel: 'Hotel Punta Cana',
+        habitacion1: 'suite',
+        habitacion2: 'doble',
+        habitacion3: 'triple',
+        servicio1: 'all inclusive',
+        servicio2: 'media pension',
+        servicio3: 'desayuno'
+    },
+    {
+        hotel: 'Hotel Dominica',
+        habitacion1: 'suite',
+        habitacion2: 'doible',
+        habitacion3: 'triple',
+        servicio1: 'all inclusive',
+        servicio2: 'media pension',
+        servicio3: 'desayuno'
+    },
+    {
+        hotel: 'Hotel Grand Bavaro',
+        habitacion1: 'suite',
+        habitacion2: 'doible',
+        habitacion3: 'triple',
+        servicio1: 'all inclusive',
+        servicio2: 'media pension',
+        servicio3: 'desayuno'
+    },
+];
